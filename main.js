9db618ecce15ec8834b8a08935748d2ded1c1635
colin <colin.williams.dev@gmail.com>
2024-05-12 17:33:42 -0500

    feat: v-for lists, keys

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [
+                { size: "xs", id: "0001" },
+                { size: "sm", id: "0002" },
+                { size: "md", id: "0003" },
+                { size: "lg", id: "0004" },
+                { size: "xl", id: "0005" }
+            ]
         }
     }
 })
