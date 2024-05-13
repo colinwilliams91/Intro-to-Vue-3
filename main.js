47f8484a25d82cf521534bccf352e10c1d541267
colin <colin.williams.dev@gmail.com>
2024-05-12 21:30:52 -0500

    feat: emptyCart computed property and remove from cart by id emitter

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
+        removeFromCart(id) {
+            const index = this.cart.indexOf(id);
+            if (index > -1) {
+                this.cart.splice(index, 1);
+            }
+        }
+    },
+    computed: {
+        emptyCart() {
+            return this.cart.length <= 0 ? true : false;
+        }
+    }
 })
